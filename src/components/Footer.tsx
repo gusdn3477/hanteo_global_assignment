7e2e3ec8a997663a207c5cf2d7e2e4dda91c5c28
박현우 <gusdn3477@naver.com>
2025-01-29 13:24:48 +0900

    feat: Footer 컴포넌트 구현

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="flex flex-col gap-8 bg-[#f8f9fa] px-4 py-6">
+      <span className="text-neutral-secondary text-xs">© CJ ENM. All Rights Reserved.</span>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/data/headerMenu.ts
@@ -0,0 +1,32 @@
+export const headerMenu = [
+  {
+    id: 1,
+    title: '챠트',
+    url: 'charts',
+  },
+  {
+    id: 2,
+    title: 'Whook',
+    url: 'whook',
+  },
+  {
+    id: 3,
+    title: '이벤트',
+    url: 'events',
+  },
+  {
+    id: 4,
+    title: '뉴스',
+    url: 'news',
+  },
+  {
+    id: 5,
+    title: '스토어',
+    url: 'store',
+  },
+  {
+    id: 6,
+    title: '충전소',
+    url: 'charge',
+  },
+] as const;
