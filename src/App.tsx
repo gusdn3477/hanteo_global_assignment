a26ef2dd0ce80d009ee6e6e2115b5bde4c514957
박현우 <gusdn3477@naver.com>
2025-01-29 13:31:35 +0900

    feat: Carousel 배치 & 자잘한 스타일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,26 @@
-import { useState } from 'react';
+import Carousel from './components/Carousel';
+import { headerMenu } from './data/headerMenu';
 
-const headerMenu = [
-  {
-    id: 1,
-    title: '챠트',
-    url: 'charts',
-  },
-  {
-    id: 2,
-    title: 'Whook',
-    url: 'whook',
-  },
-  {
-    id: 3,
-    title: '이벤트',
-    url: 'events',
-  },
-  {
-    id: 4,
-    title: '뉴스',
-    url: 'news',
-  },
-  {
-    id: 5,
-    title: '스토어',
-    url: 'store',
-  },
-  {
-    id: 6,
-    title: '충전소',
-    url: 'charge',
-  },
-];
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="flex h-full w-full flex-col">
-      <header className="h-[54px] bg-red-200">
-        <ul className="flex h-full">
+      <header className="h-[54px] bg-red-200 p-2">
+        <ul className="flex h-full justify-center gap-4">
           {headerMenu.map((menu) => (
-            <li key={menu.id} className="flex items-center">
+            <li key={menu.id} className="flex items-center font-extrabold">
               {menu.title}
             </li>
           ))}
         </ul>
       </header>
-      <div className="h-[200px]">Carousel</div>
+      <div className="h-[200px]">
+        <Carousel />
+      </div>
       <div className="flex-1 overflow-y-auto bg-gray-200">콘텐츠 큐레이션</div>
 
-      <footer className="h-[74px] bg-gray-300">푸터</footer>
+      <footer className="flex gap-8 bg-[#f8f9fa] px-4 py-4">
+        <span className="text-neutral-secondary text-xs">© CJ ENM. All Rights Reserved.</span>
+      </footer>
     </div>
   );
 }
