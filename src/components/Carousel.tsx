588df9cd755c0750e81e6debf6a98f78226c6fce
박현우 <gusdn3477@naver.com>
2025-01-30 14:33:30 +0900

    feat: Carousel에 이미지 경로 추가 & 자잘한 스타일 수정

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -14,7 +14,7 @@ const Carousel = () => {
     slidesToShow: 1, // 한 번에 보여줄 슬라이드 수
     slidesToScroll: 1, // 한 번에 스크롤할 슬라이드 수
     centerMode: true, // 현재 슬라이드를 가운데 정렬
-    centerPadding: '15px', // 양쪽에 보일 카드의 너비
+    centerPadding: '40px', // 양쪽에 보일 카드의 너비
     arrows: false,
     dots: true,
   };
@@ -22,20 +22,22 @@ const Carousel = () => {
   return (
     <MySlider {...settings}>
       {cardEvents.map((card) => (
-        <div key={card.id} className="mt-12">
+        <div key={card.id} className="mt-3">
           <div className="flex items-center justify-center">
-            <div className="relative">
-              {/* <img
-                src={album.nfcImageUrl || ''}
-                alt={card.title}
-                className="h-[457px] w-[290px] rounded-2xl"
-              /> */}
-            </div>
+            <img
+              src={`${card.imageUrl}` || ''}
+              alt={card.title}
+              className="h-[160px] w-full rounded-2xl"
+            />
           </div>
           <section className="mt-3 flex flex-col">
             <div className="flex w-full items-center justify-between">
-              <strong className="w-3/4 truncate">{card.title}</strong>
-              <button className="rounded-full border border-red-400 text-red-400">투표하기</button>
+              <strong className="flex-1 truncate">{card.title}</strong>
+              {card.id === 1 && (
+                <button className="w-[70px] rounded-full border border-red-400 text-red-400">
+                  투표하기
+                </button>
+              )}
             </div>
             <p className="flex justify-end text-sm text-[#6C6C6C]">{card.date}</p>
           </section>
