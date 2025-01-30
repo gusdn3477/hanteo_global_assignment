26e24965feb7f89389f154f6eb26c6d26d1e50ee
박현우 <gusdn3477@naver.com>
2025-01-30 17:18:03 +0900

    feat: 스와이프 허용하는 SwipeNavigator Router.tsx에 배치

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,24 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Charts from './Charts';
 import Layout from './components/Layout';
+import SwipeNavigator from './components/SwipeNavigator';
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/charts" />} />
-        <Route element={<Layout />}>
-          <Route path="/charts" element={<Charts />} />
-        </Route>
-      </Routes>
+      <SwipeNavigator>
+        <Routes>
+          <Route path="/" element={<Navigate to="/charts" />} />
+          <Route element={<Layout />}>
+            <Route path="/charts" element={<Charts />} />
+            <Route path="/whook" element={<div className="flex-1">whook 페이지</div>} />
+            <Route path="/events" element={<div className="flex-1">events 페이지</div>} />
+            <Route path="/news" element={<div className="flex-1">news 페이지</div>} />
+            <Route path="/store" element={<div className="flex-1">store 페이지</div>} />
+            <Route path="/charge" element={<div className="flex-1">charge 페이지</div>} />
+          </Route>
+        </Routes>
+      </SwipeNavigator>
     </BrowserRouter>
   );
 }
